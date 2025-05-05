789b6fa73c2d285f9c859121dc5c88cc1b7dcfe7
recla93 <recla93@gmail.com>
2025-05-05 13:00:22 +0200

    Start

---
+++ src/main/java/org/example/dandd/DandDApplication.java
@@ -0,0 +1,15 @@
+package org.example.dandd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DandDApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DandDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/dandd/DandDApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.dandd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DandDApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
